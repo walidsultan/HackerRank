dd22bff21df86e58d01f15f4fd9d7a9108c8ab84
walidsultan <walidalysultan@gmail.com>
2015-01-17 16:01:40 +0200

    Solved PRNGSequenceGuessing problem in CryptoGraphy

---
+++ Algorithms/Cryptography/PRNGSequenceGuessing.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Algorithms.Cryptography
+{
+    public class PRNGSequenceGuessing
+    {
+        private IDisplayHandler _console;
+
+        public PRNGSequenceGuessing(IDisplayHandler console)
+        {
+            _console = console;
+        }
+
+        public void Solve()
+        {
+            int T = int.Parse(_console.ReadLine());
+            for (int t = 0; t < T; t++)
+            {
+                string[] seedRange = _console.ReadLine().Split(' ');
+
+                int[] expectedOutput = new int[10];
+                for (int i = 0; i < 10; i++)
+                {
+                    expectedOutput[i] = int.Parse(_console.ReadLine());
+                }
+
+                int startSeed = int.Parse(seedRange[0]);
+                int stopSeed = int.Parse(seedRange[1]);
+
+                
+                for (int i = 0; i < (stopSeed - startSeed); i++)
+                {
+                    Random random = new Random(startSeed + i);
+                    string output = string.Empty;
+                    bool isCorrectSeed = true;
+                    for (int j = 0; j < 10; j++)
+                    {
+                        int actualOutput = random.Next (1000);
+                        if (expectedOutput[j] != actualOutput)
+                        {
+                            isCorrectSeed = false;
+                            break;
+                        }
+                    }
+                    //Got the correct seed
+                    if (isCorrectSeed)
+                    {
+                        output = (startSeed + i).ToString();
+                        for (int z = 0; z < 10; z++)
+                        {
+                            output += " " + random.Next(1000).ToString();
+                        }
+
+                        if (!string.IsNullOrEmpty(output))
+                        {
+                            _console.WriteLine(output);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AlgorithmsTests/Cryptography/PRNGSequenceGuessing/PRNGSequenceGuessingTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HackerRank.Algorithms;
+using HackerRank.Algorithms.Cryptography;
+
+namespace HackerRank.AlgorithmsTests.Cryptography
+{
+    [TestClass]
+    public class PRNGSequenceGuessingTests
+    {
+        [TestMethod]
+        public void PRNGSequenceGuessing_Test01()
+        {
+            string[] testData = { "1", "1374037200 1374123600", "643", "558", "37", "259", "913", "939", "101", "569", "607", "974"};
+
+            ConsoleTester consoleTester = new ConsoleTester(testData);
+
+            PRNGSequenceGuessing prngSequenceGuessing = new PRNGSequenceGuessing(consoleTester);
+
+            prngSequenceGuessing.Solve();
+
+            Assert.AreEqual("1374037200 59 721 676 927 438 714 596 342 537 758", consoleTester._outputBuffer[0], "Test01 is wrong");
+        }
+    }
+}
