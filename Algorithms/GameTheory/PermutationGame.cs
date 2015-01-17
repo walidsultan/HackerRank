39f167d9a9a46ce188e560055add4913b45657d9
walidsultan <walidalysultan@gmail.com>
2015-01-17 18:35:13 +0200

    Added permutation game test cases

---
+++ Algorithms/GameTheory/PermutationGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Algorithms.GameTheory
+{
+    public class PermutationGame
+    {
+        private IDisplayHandler _console;
+
+        public PermutationGame(IDisplayHandler console)
+        {
+            _console = console;
+        }
+
+        public void Solve()
+        { }
+    }
+}
---
+++ AlgorithmsTests/GameTheory/PermutationGame/PermutationGameTests.cs
@@ -0,0 +1,29 @@
+﻿using HackerRank.Algorithms;
+using HackerRank.Algorithms.GameTheory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.AlgorithmsTests.GameTheory
+{
+    [TestClass]
+    public class PermutationGameTests
+    {
+        [TestMethod]
+        public void PermutationGameTests_Test01()
+        {
+            string[] testData = { "2", "3", "1 3 2", "5", "5 3 2 1 4" };
+
+            ConsoleTester consoleTester = new ConsoleTester(testData);
+
+            PermutationGame permutationGame = new PermutationGame(consoleTester);
+
+            permutationGame.Solve();
+            Assert.AreEqual(consoleTester._outputBuffer[0], "Alice", "Test01 is wrong");
+            Assert.AreEqual(consoleTester._outputBuffer[1], "Bob", "Test01 is wrong");
+        }
+    }
+}
