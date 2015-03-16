cdd8a72059c6a9098604b4a3677fbe44f2bdfe52
Walid Sultan <walidalysultan@gmail.com>
2015-03-16 19:51:09 +0200

    Solved restaurants algorithm in Number Theory

---
+++ Algorithms/NumberTheory/Restaurant.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Algorithms.NumberTheory
+{
+    public class Restaurant
+    {
+        private IDisplayHandler _console;
+
+        public Restaurant(IDisplayHandler console)
+        {
+            _console = console;
+        }
+        public void Solve()
+        {
+            int T = int.Parse(_console.ReadLine());
+            for (int t = 0; t < T; t++)
+            {
+                string[] inParams = _console.ReadLine().Split(' ');
+                int l = int.Parse(inParams[0]);
+                int b = int.Parse(inParams[1]);
+
+                int largestSide = Math.Min(l, b);
+                int totalArea = l * b;
+
+                for (int i = largestSide; i > 0; i--)
+                {
+                    int s = i * i;
+                    if (totalArea % s == 0 && l % i == 0 && b % i == 0)
+                    {
+                        int numberOfSquares = totalArea / (i * i);
+                        _console.WriteLine(numberOfSquares.ToString());
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AlgorithmsTests/NumberTheory/RestaurantTests.cs
@@ -0,0 +1,29 @@
+﻿using HackerRank.Algorithms;
+using HackerRank.Algorithms.NumberTheory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.AlgorithmsTests.NumberTheory
+{
+    [TestClass]
+    public class RestaurantTests
+    {
+        [TestMethod]
+        public void Restaurant_Test01()
+        {
+            string[] testData = { "11", "38 751", "344 734", "165 635", "297 667", "917 264", "544 642", "254 914", "612 50", "94 707", "564 417", "145 246" };
+
+            ConsoleTester consoleTester = new ConsoleTester(testData);
+
+            Restaurant restaurant = new Restaurant(consoleTester);
+
+            restaurant.Solve();
+            Assert.AreEqual(consoleTester._outputBuffer[0], "28538", "Test01 is wrong");
+            Assert.AreEqual(consoleTester._outputBuffer[1], "63124", "Test01 is wrong");
+        }
+    }
+}
