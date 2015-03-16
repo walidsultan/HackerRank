b93ba0f2515e0dfa39c4f614b84caf83c8c655f9
Walid Sultan <walidalysultan@gmail.com>
2015-03-16 20:24:29 +0200

    Added Matrix Tree partial solution

---
+++ Algorithms/NumberTheory/MatrixTree.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Algorithms.NumberTheory
+{
+    public class MatrixTree
+    {
+        private IDisplayHandler _console;
+        public MatrixTree(IDisplayHandler console)
+        {
+            _console = console;
+        }
+
+        public void Solve()
+        {
+            int N = int.Parse(_console.ReadLine());
+            int[] W = _console.ReadLine().Split(' ').Select(s => int.Parse(s)).ToArray();
+
+            for (int i = 0; i < N-1; i++)
+            {
+                //Read edges
+
+            }
+        }
+    }
+}
