0bc9559212ebfdfadfc56eb3b000630a59585a85
Walid Sultan <walidalysultan@gmail.com>
2015-03-17 23:56:25 +0200

    Solved GemStones

---
+++ Algorithms/Strings/GemStones.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Algorithms.Strings
+{
+    public class GemStones
+    {
+        private IDisplayHandler _console;
+        public GemStones(IDisplayHandler console)
+        {
+            _console = console;
+        }
+
+        public void Solve()
+        {
+            int N = int.Parse(_console.ReadLine());
+            int[] a = new int[26];
+            int offset = 97;
+            for (int n = 0; n < N; n++)
+            {
+                string s = _console.ReadLine();
+                foreach (char c in s.ToCharArray().Distinct())
+                {
+                    int index = (int)c - offset;
+                    a[index]++;
+                }
+            }
+
+            int gemStones = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] == N)
+                {
+                    gemStones++;
+                }
+            }
+
+            _console.WriteLine(gemStones.ToString());
+        }
+    }
+}
---
+++ AlgorithmsTests/Strings/GemStones/GemStonesTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HackerRank.Algorithms;
+using HackerRank.Algorithms.Strings;
+using System.IO;
+
+namespace HackerRank.AlgorithmsTests.Strings
+{
+    [TestClass]
+    public class GemStonesTests
+    {
+        [TestMethod]
+        public void GemStones_Test01()
+        {
+            string[] testData = { "3", "abcdde", "baccd", "eeabg" };
+            
+            ConsoleTester consoleTester = new ConsoleTester(testData);
+
+            GemStones gemStones = new GemStones(consoleTester);
+
+            gemStones.Solve();
+
+            Assert.AreEqual("2", consoleTester._outputBuffer[0], "Test01 is wrong");
+        }
+
+        [TestMethod]
+        public void GemStones_Test00()
+        {
+            string[] stringSeparators = new string[] { "\n" };
+            string[] testData = File.ReadAllText("Strings\\GemStones\\input00.txt").Split(stringSeparators, StringSplitOptions.None);
+
+            ConsoleTester consoleTester = new ConsoleTester(testData);
+
+            GemStones gemStones = new GemStones(consoleTester);
+
+            gemStones.Solve();
+
+            string[] outputData = File.ReadAllText("Strings\\GemStones\\output00.txt").Split(stringSeparators, StringSplitOptions.None);
+
+            for (int i = 0; i < outputData.Length; i++)
+            {
+                Assert.AreEqual(consoleTester._outputBuffer[i], outputData[i], string.Format("Test00 is wrong for test {0}", i));
+            }
+        }
+    }
+}
