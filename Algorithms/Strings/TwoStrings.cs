fd0a5641280b0685460e02bbc3c386e5dad943ea
Walid Sultan <walidalysultan@gmail.com>
2015-03-18 22:37:59 +0200

    Added twoStrings algorithm

---
+++ Algorithms/Strings/TwoStrings.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+
+namespace HackerRank.Algorithms.Strings
+{
+    public class TwoStrings
+    {
+        private IDisplayHandler _console;
+        public TwoStrings(IDisplayHandler console)
+        {
+            _console = console;
+        }
+
+        public void Solve()
+        {
+            int T = int.Parse(_console.ReadLine());
+            for (int t = 0; t < T; t++)
+            {
+                string s1 = _console.ReadLine();
+                string s2 = _console.ReadLine();
+
+                bool isfound = false;
+                foreach (char c in s1.Distinct())
+                {
+                    if (s2.Contains(c))
+                    {
+                        isfound = true;
+                        break;
+                    }
+                }
+
+                if (isfound)
+                {
+                    _console.WriteLine("YES");
+                }
+                else
+                {
+                    _console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
---
+++ AlgorithmsTests/Strings/TwoStrings/TwoStringsTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HackerRank.Algorithms;
+using HackerRank.Algorithms.Strings;
+using System.IO;
+
+namespace HackerRank.AlgorithmsTests.Strings
+{
+    [TestClass]
+    public class TwoStringsTests
+    {
+        [TestMethod]
+        public void TwoStringsTests_Test03()
+        {
+            string[] stringSeparators = new string[] { "\n" };
+            string[] testData = File.ReadAllText("Strings\\TwoStrings\\input03.txt").Split(stringSeparators, StringSplitOptions.None);
+
+            ConsoleTester consoleTester = new ConsoleTester(testData);
+
+            TwoStrings twoStrings = new TwoStrings(consoleTester);
+
+            twoStrings.Solve();
+
+            string[] outputData = File.ReadAllText("Strings\\TwoStrings\\output03.txt").Split(stringSeparators, StringSplitOptions.None);
+
+            for (int i = 0; i < outputData.Length; i++)
+            {
+                Assert.AreEqual(consoleTester._outputBuffer[i], outputData[i], string.Format("Test03 is wrong for test {0}", i));
+            }
+        }
+    }
+}
