463c38ca8fbfb708f7405717feb9e0d87272dfb6
Walid Sultan <walidalysultan@gmail.com>
2015-03-19 19:05:12 +0200

    Added reverse game

---
+++ Algorithms/NumberTheory/ReverseGame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Algorithms.NumberTheory
+{
+   public class ReverseGame
+    {
+          private IDisplayHandler _console;
+
+          public ReverseGame(IDisplayHandler console)
+        {
+            _console = console;
+        }
+
+          public void Solve()
+          {
+              int T = int.Parse(_console.ReadLine());
+              for (int t = 0; t < T; t++)
+              {
+                  int[] test = _console.ReadLine().Split(' ').Select(s => int.Parse(s)).ToArray();
+                  int n = test[0] - 1;
+                  int k = test[1];
+                  int index = 0;
+                  if (k >=  (float) n / 2)
+                  {
+                      index = (n - k) * 2;
+                  }
+                  else
+                  {
+                      index = 2 * k + 1;
+                  }
+                  _console.WriteLine(index.ToString());
+              }
+          }
+    }
+}
---
+++ AlgorithmsTests/NumberTheory/ReverseGame/ReverseGameTests.cs
@@ -0,0 +1,36 @@
+﻿using HackerRank.Algorithms;
+using HackerRank.Algorithms.NumberTheory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.AlgorithmsTests.NumberTheory
+{
+    [TestClass]
+    public class ReverseGameTests
+  {
+        [TestMethod]
+        public void ReverseGame_Test01()
+        {
+            string[] stringSeparators = new string[] { "\r\n", "\r", "\n" };
+            string[] testData = File.ReadAllText("NumberTheory\\ReverseGame\\input01.txt").Split(stringSeparators, StringSplitOptions.None);
+
+            ConsoleTester consoleTester = new ConsoleTester(testData);
+
+            ReverseGame reverseGame = new ReverseGame(consoleTester);
+
+            reverseGame.Solve();
+
+            string[] outputData = File.ReadAllText("NumberTheory\\ReverseGame\\output01.txt").Split(stringSeparators, StringSplitOptions.None);
+
+            for (int i = 0; i < outputData.Length; i++)
+            {
+                Assert.AreEqual(outputData[i], consoleTester._outputBuffer[i], string.Format("Test01 is wrong for test {0}", i));
+            }
+        }
+    }
+}
