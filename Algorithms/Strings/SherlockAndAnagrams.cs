a1e3ca1279336de5b9a53e37ff6564d74beeb02b
Walid Sultan <walidalysultan@gmail.com>
2015-03-21 14:46:02 +0200

    Solved sherlock and anagrams in strings

---
+++ Algorithms/Strings/SherlockAndAnagrams.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Algorithms.Strings
+{
+    public class SherlockAndAnagrams
+    {
+        private IDisplayHandler _console;
+        public SherlockAndAnagrams(IDisplayHandler console)
+        {
+            _console = console;
+        }
+
+        public void Solve()
+        {
+            int T = int.Parse(_console.ReadLine());
+            for (int t = 0; t < T; t++)
+            {
+                string s = _console.ReadLine();
+                int anagrams = 0;
+                for (int x = 1; x < s.Length ; x++)
+                {
+                    for (int i = 0; i < (s.Length - x+1); i++)
+                    {
+                        string si = s.Substring(i, x);
+                        for (int j = 0; j < (s.Length - x+1); j++)
+                        {
+                            if (i != j)
+                            {
+                                string sj = s.Substring(j, x);
+                                if (IsAnagram(si,sj))
+                                {
+                                    anagrams++;
+                                }
+                            }
+                        }
+                    }
+                }
+                _console.WriteLine((anagrams/2).ToString());
+            }
+        }
+
+        public bool IsAnagram(string s1, string s2)
+        {
+            int[] counter = new int[26];
+            for (int i = 0; i < s1.Length; i++)
+            {
+                counter[(int)s1[i] - 97]++;
+                counter[(int)s2[i] - 97]--;
+            }
+            int positiveCount = 0;
+            counter.ToList().ForEach(a =>
+            {
+                if (a > 0)
+                {
+                    positiveCount += a;
+                }
+            });
+            return positiveCount==0;
+        }
+    }
+}
---
+++ AlgorithmsTests/Strings/SherlockAndAnagrams/SherlockAndAnagramsTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HackerRank.Algorithms;
+using HackerRank.Algorithms.Strings;
+using System.IO;
+
+namespace HackerRank.AlgorithmsTests.Strings
+{
+    [TestClass]
+    public class SherlockAndAnagramsTests
+    {
+        [TestMethod]
+        public void SherlockAndAnagrams_Test01()
+        {
+            string[] testData = { "2", "abba", "abcd" };
+
+            ConsoleTester consoleTester = new ConsoleTester(testData);
+
+            SherlockAndAnagrams sherlockAndAnagrams = new SherlockAndAnagrams(consoleTester);
+
+            sherlockAndAnagrams.Solve();
+
+            Assert.AreEqual("4", consoleTester._outputBuffer[0], "Test01 is wrong");
+            Assert.AreEqual("0", consoleTester._outputBuffer[1], "Test01 is wrong");
+        }
+
+        [TestMethod]
+        public void SherlockAndAnagrams_Test02()
+        {
+            string[] stringSeparators = new string[] { "\r\n", "\r", "\n" };
+            string[] testData = File.ReadAllText("Strings\\SherlockAndAnagrams\\input01.txt").Split(stringSeparators, StringSplitOptions.None);
+
+            ConsoleTester consoleTester = new ConsoleTester(testData);
+
+            SherlockAndAnagrams sherlockAndAnagrams = new SherlockAndAnagrams(consoleTester);
+
+            sherlockAndAnagrams.Solve();
+
+            string[] outputData = File.ReadAllText("Strings\\SherlockAndAnagrams\\output01.txt").Split(stringSeparators, StringSplitOptions.None);
+
+            for (int i = 0; i < outputData.Length; i++)
+            {
+                Assert.AreEqual(outputData[i], consoleTester._outputBuffer[i], string.Format("Test02 is wrong for test {0}", i));
+            }
+        }
+    }
+}
