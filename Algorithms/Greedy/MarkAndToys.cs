1238cde4762adcfc4697d709ec2b96194f9d8e5d
Walid Sultan <walidalysultan@gmail.com>
2015-03-22 13:34:11 +0200

    Solved Mark and Toys in greedy

---
+++ Algorithms/Greedy/MarkAndToys.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Algorithms.Greedy
+{
+  public  class MarkAndToys
+    {
+      private IDisplayHandler _console;
+      public MarkAndToys(IDisplayHandler console)
+        {
+            _console = console;
+        }
+      public void Solve()
+      {
+          int[] T = _console.ReadLine().Split(' ').Select(s => int.Parse(s)).ToArray();
+          int N = T[0];
+          int K = T[1];
+
+          int[] arr = _console.ReadLine().Split(' ').Select(s => int.Parse(s)).OrderBy(s=>s).ToArray();
+
+          int toysCost = 0;
+          for (int i = 0; i<arr.Length; i++)
+          {
+              toysCost += arr[i];
+              if (toysCost > K)
+              {
+                  _console.WriteLine(i);
+                  break;
+              }
+          }
+      }
+    }
+}
