095db7dbbdaea965fb3451159f8dfb030d8081e2
Walid Sultan <walidalysultan@gmail.com>
2015-03-31 16:42:51 +0200

    Solved Priyanka and Toys in Greedy

---
+++ Algorithms/Greedy/PriyankaAndToys.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.Algorithms.Greedy
+{
+    public class PriyankaAndToys
+    {
+        private IDisplayHandler _console;
+        public PriyankaAndToys(IDisplayHandler console)
+        {
+            _console = console;
+        }
+        public void Solve()
+        {
+            int N = int.Parse(_console.ReadLine());
+            int[] arr = _console.ReadLine().Split(' ').Select(s => int.Parse(s)).OrderBy(s=>s).ToArray();
+
+            int w = arr[0];
+            int p = 1;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] - w > 4)
+                {
+                    p++;
+                    w = arr[i];
+                }
+            }
+            _console.WriteLine(p.ToString());
+        }
+    }
+}
---
+++ AlgorithmsTests/Greedy/PriyankaAndToys/PriyankaAndToysTests.cs
@@ -0,0 +1,37 @@
+﻿using HackerRank.Algorithms;
+using HackerRank.Algorithms.Greedy;
+using HackerRank.Algorithms.NumberTheory;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank.AlgorithmsTests.NumberTheory
+{
+    [TestClass]
+    public class PriyankaAndToysTests
+  {
+        [TestMethod]
+        public void PriyankaAndToys_Test00()
+        {
+            string[] stringSeparators = new string[] { "\r\n", "\r", "\n" };
+            string[] testData = File.ReadAllText("Greedy\\PriyankaAndToys\\input01.txt").Split(stringSeparators, StringSplitOptions.None);
+
+            ConsoleTester consoleTester = new ConsoleTester(testData);
+
+            PriyankaAndToys priyankaAndToys = new PriyankaAndToys(consoleTester);
+
+            priyankaAndToys.Solve();
+
+            string[] outputData = File.ReadAllText("Greedy\\PriyankaAndToys\\output01.txt").Split(stringSeparators, StringSplitOptions.None);
+
+            for (int i = 0; i < outputData.Length; i++)
+            {
+                Assert.AreEqual(outputData[i], consoleTester._outputBuffer[i], string.Format("Test00 is wrong for test {0}", i));
+            }
+        }
+    }
+}
